530e3e076fcec20c4161fce93de3987755f4b853
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2026-02-26 19:32:25 +0200

    Update docs; Add additonal game information;

--- src/game.py
+++ src/game.py
@@ -7,4 +7,8 @@ class GameData():
         self.mode = "minimax" # alfa-beta
         self.start = 0 # 0 is player
         self.number = config['gen_algorithm']
+        self.nodes = []
+        self.player_score_1 = 0
+        self.player_score_2 = 0
         print("Game Script is running")
+
