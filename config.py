4405394cec7cfc26ee31c098c3e6af342cf3a884
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2026-02-26 19:02:52 +0200

    Additonal config settings;

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 config = {
     "divisors": [2, 3, 4],
     "bank_divisors": [5, 10],
-    "gen_algorithm": "simple"
+    "gen_algorithm": "simple",
+    "upper_bound": 20000,
+    "lower_bound": 30000
 }
