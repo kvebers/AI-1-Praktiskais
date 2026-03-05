51504fc605064f7e97242dd2be39b8871df54331
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2026-03-05 18:57:51 +0200

    Update: Settings page, generation and data updates for the game;

--- src/number_gen.py
+++ src/number_gen.py
@@ -7,26 +7,26 @@ low_bound=config['upperBound']
 up_bound=config['lowerBound']
 
 
-def generate_random(up=low_bound, low=up_bound):
+def generateRandom(up=low_bound, low=up_bound):
     return random.randint(up, low)
     
-def simple_gen():
-    rand = generate_random()
+def simpleGen():
+    rand = generateRandom()
     while (rand % 2 != 0 or rand % 3 != 0):
-        rand = generate_random()
+        rand = generateRandom()
     return rand
 
 
-def one_nine_gen():
-    value = generate_random(1, 9)
-    multi = generate_random(2, 4)
+def oneNineGen():
+    value = generateRandom(1, 9)
+    multi = generateRandom(2, 4)
     while (value * multi < up_bound):
         value *= multi
-        multi = generate_random(2, 4)
+        multi = generateRandom(2, 4)
         if (value >= low_bound and value <= up_bound):
             break
     if (value <= low_bound or value >= up_bound):
-        value = one_nine_gen()
+        value = oneNineGen()
     return value
 
 
